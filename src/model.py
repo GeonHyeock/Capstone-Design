b095fc431178f8e5a4c42f74d832547ed01e31d5
GeonHyeock <gunheock@gmail.com>
2023-05-11 19:34:42 +0900

    Feat : preprocessing

--- src/model.py
+++ src/model.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class Regression:
     ## TODO : 선형회귀
     ## TODO : Ridge
@@ -16,9 +19,16 @@ class Classification:
     pass
 
 
-def preprocessing():
-    ## TODO : 데이터 전처리, 결측치 제거, 정규화? one-hot
-    pass
+def preprocessing(data_path="../data/Expanded_data_with_more_features.csv"):
+    """결측치 제거
+
+    Returns:
+        data_path: 데이터 주소
+    """
+    data = pd.read_csv(data_path, index_col=0)
+    data.dropna(inplace=True)  # 11398 삭제
+    data.reset_index(drop=True, inplace=True)
+    return data
 
 
 if __name__ == "__main__":
