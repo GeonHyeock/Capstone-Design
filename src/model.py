2df51d6e80132aad3c480909d2a42178e893eb24
GeonHyeock <gunheock@gmail.com>
2023-05-11 19:03:28 +0900

    Add : model TODO list

--- src/model.py
+++ src/model.py
@@ -1,10 +1,25 @@
 class Regression:
+    ## TODO : 선형회귀
+    ## TODO : Ridge
+    ## TODO : Lasso
+    ## TODO : PCR
+    ## TODO : PLS
+    ## TODO : subset selection
     pass
 
 
 class Classification:
+    ## TODO : LDA & QDA & RDA
+    ## TODO : Logistic Regression
+    ## TODO : SVM
+    ## TODO : TREE
     pass
 
 
 def preprocessing():
+    ## TODO : 데이터 전처리, 결측치 제거, 정규화? one-hot
+    pass
+
+
+if __name__ == "__main__":
     pass
