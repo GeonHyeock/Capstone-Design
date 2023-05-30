a906e38c6fcbf52816a821b262514283bedcd75e
GeonHyeock <gunheock@gmail.com>
2023-05-30 21:00:02 +0900

    Feat : result visual

--- src/visual.py
+++ src/visual.py
@@ -30,7 +30,7 @@ def EDA(data_path):
     plt.savefig(f"./images/EDA_gender_MathScore.png")
     plt.clf()
 
-    for col in data.drop(["ReadingScore", "WritingScore"], axis=1).columns:
+    for col in data.drop(["ReadingScore", "WritingScore", "MathScore"], axis=1).columns:
         sns.boxplot(data=data, x=col, y="MathScore").set(title=f"{col}에 따른 수학점수")
         plt.savefig(f"./images/EDA_{col}.png")
         plt.clf()
@@ -42,13 +42,17 @@ def EDA(data_path):
 
 
 def classification_result(result):
-    for col in set(result.keys()) & set(["LDA", "QDA", "Logistic Regression", "Tree"]):
+    for col in set(result.keys()) & set(
+        ["LDA", "QDA", "Logistic Classification", "Tree"]
+    ):
         data = result[col]
         plot = sns.heatmap(
             data["metric"] / data["metric"].sum(axis=1),
             annot=True,
             fmt=".2f",
             cmap="Blues",
+            xticklabels=[f"{i} 등급" for i in range(1, 10)],
+            yticklabels=[f"{i} 등급" for i in range(1, 10)],
         )
         best_param = {
             k: v if isinstance(v, str) else f"{v:.2f}"
@@ -60,13 +64,38 @@ def classification_result(result):
         plt.clf()
         print(f"{col}분류율 : {np.trace(data['metric']) / np.sum(data['metric']):.4f}")
 
+        if col in ["QDA", "Logistic Classification"]:
+            if col == "QDA":
+                param = "param_reg_param"
+                data_dict = {
+                    "train_score": data["cv_result"]["mean_train_score"],
+                    "test_score": data["cv_result"]["mean_test_score"],
+                    f"{param}": data["cv_result"][param].data,
+                }
+                pd.DataFrame(data_dict).plot.line(x=f"{param}")
+            else:
+                idx = (
+                    data["cv_result"]["param_penalty"] == data["best_param"]["penalty"]
+                ) & (data["cv_result"]["param_solver"] == data["best_param"]["solver"])
+                param = "param_C"
+                data_dict = {
+                    "train_score": data["cv_result"]["mean_train_score"][idx],
+                    "test_score": data["cv_result"]["mean_test_score"][idx],
+                    f"{param}": data["cv_result"][param].data[idx],
+                }
+                pd.DataFrame(data_dict).plot.line(x=f"{param}")
+                plt.xlim(0, 1)
+            plt.savefig(f"./images/{col}_{param}.png")
+            plt.clf()
+
 
 def regression_result(result):
     for col in set(result.keys()) & set(["LinearRegression", "Ridge", "LASSO", "PLS"]):
         data = result[col]
-        print(f"{col} : {data['metric']:.4f}")
-        print(f"{col} : {data['best_param']}")
+        _, b, c = data["metric"]
+        print(f"{col} : {np.sqrt(((b-c)**2).mean()):.4f}")
         if col in ["Ridge", "LASSO", "PLS"]:
+            print(f"{col} : {data['best_param']}")
             param = "param_n_components" if col == "PLS" else "param_alpha"
             data_dict = {
                 "train_score": data["cv_result"]["mean_train_score"],
@@ -74,6 +103,7 @@ def regression_result(result):
                 f"{param}": data["cv_result"][param].data,
             }
             pd.DataFrame(data_dict).plot.line(x=f"{param}")
+            plt.ylim(0.5, 1)
             plt.savefig(f"./images/{col}_{param}.png")
             plt.clf()
 
@@ -81,7 +111,7 @@ def regression_result(result):
 if __name__ == "__main__":
     plt.rcParams["font.family"] = "AppleGothic"
     my_dm = DM()
-    with open("./result4.pickle", "rb") as fr:
+    with open("./result_no_expension.pickle", "rb") as fr:
         result = pickle.load(fr)
 
     EDA("data/Expanded_data_with_more_features.csv")
